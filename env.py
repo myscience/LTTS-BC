Fixed bug in env extent and scale computation

env.py
@@ -17,12 +17,12 @@ class Reach:
         wheter an episode is concluded, this can be for example occur when agent
         exits the bounds or target is reached or total simulation time expires.
     '''
-    def __init__(self, max_T = 100, targ = None, init = None, extent = ((-1, 1), (1, 1)), render = True):
+    def __init__(self, max_T = 100, targ = None, init = None, extent = ((-1, -1), (1, 1)), render = True):
         # Here we collect environment duration and span
         self.max_T = max_T;
-        self.extent = extent;
-        self.scale = np.mean (extent)
-        self.inv_scale = 1 / np.mean (extent);
+        self.extent = np.array (extent);
+        self.scale = np.mean (self.extent[1] - self.extent[0])
+        self.inv_scale = 1 / self.scale;
 
         # Keep internal time
         self.t = 0;